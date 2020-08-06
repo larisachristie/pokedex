5173815567fb293e9987acc87b6ac744f5b1f98f
larisachristie <larisachristie@gmail.com>
2020-08-06 11:17:05 +0200

    Updates

---
+++ precache-manifest.0b6ecaa805590ad008f4318c42805d0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99b1e1ae599bf9bc801d99b50666134e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6877491115f81111d67d",
+    "url": "/static/css/main.96e57811.chunk.css"
+  },
+  {
+    "revision": "aeb0637ea2948c6d4804",
+    "url": "/static/js/2.c308d6d0.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/static/js/2.c308d6d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6877491115f81111d67d",
+    "url": "/static/js/main.880552fb.chunk.js"
+  },
+  {
+    "revision": "250103716dfc89e5abb8",
+    "url": "/static/js/runtime-main.9ff9079d.js"
+  }
+]);
